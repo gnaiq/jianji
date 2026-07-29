2ba4f1a37cd1889fddfac92e155420a35b9bd332
Life Bitterness <63579431+gnaiq@users.noreply.github.com>
2026-07-29 11:10:22 +0800

    chore: release v1.6.17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.jianji"
         minSdk = 24
         targetSdk = 34
-        versionCode = 49
-        versionName = "1.6.16"
+        versionCode = 50
+        versionName = "1.6.17"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
